671c37356eb36e67ae40ad33fc843c2b34c54b1e
Kanai Masumi <masumi.net@gmail.com>
2017-05-27 05:22:50 +0200

    [fix] WithCancelBySignal

--- contextutil/contextutil.go
+++ contextutil/contextutil.go
@@ -11,9 +11,9 @@ import (
 // WithCancelBySignal returns context cancelable by signal
 func WithCancelBySignal(parent context.Context, sigs ...os.Signal) (ctx context.Context) {
 	ctx, cancel := context.WithCancel(parent)
+	sc := make(chan os.Signal)
+	signal.Notify(sc, sigs...)
 	go func() {
-		sc := make(chan os.Signal)
-		signal.Notify(sc, sigs...)
 		select {
 		case <-ctx.Done():
 		case <-sc:
